adf2ee372eff2cb8a378e9385502ab8d3f07e260
threetreeslight <ae06710@gmail.com>
2019-07-24 21:37:33 +0900

    kne feat. tada

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,26 +5,33 @@ describe Game do
   let(:game){ Game.new }
 
   describe "#assert" do
+    subject{ game.assert(demes) }
     context "ひふみ" do
+      let(:demes){[3,2,1]}
       it do
-        demes = [3,2,1]
         expect(game.assert(demes)).to eq "lose"
       end
     end
 
     context "しごろ" do
+      let(:demes){[6,5,4]}
       it do
-        demes = [6,5,4]
         expect(game.assert(demes)).to eq "win"
       end
     end
 
     context "でめ" do
+      let(:demes){[2,2,1]}
       it do
-        demes = [2,2,1]
         expect(game.assert(demes)).to eq "deme1"
       end
     end
 
+    context "pinzoro" do
+      let(:demes){[1,1,1]}
+      it do
+        expect(game.assert(demes)).to eq "win"
+      end
+    end
   end
 end
