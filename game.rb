5dda540edaac328f5dca82824f253f953d950503
threetreeslight <ae06710@gmail.com>
2019-07-24 20:26:08 +0900

    yubachiri feat. itakura

--- game.rb
+++ game.rb
@@ -1,16 +1,29 @@
 require_relative "dice.rb"
 
 class Game
+  PINZORO = [1, 1, 1]
+  NIZORO = [2, 2, 2]
+  SANZORO = [3, 3, 3]
+  YONZORO = [4, 4, 4]
+  GOZORO = [5, 5, 5]
+  ROKUZORO = [6, 6, 6]
+  HIFUMI = [1, 2, 3]
+  SHIGORO = [4, 5, 6]
+
   def main
-    puts furu
+    puts roll
   end
 
-  def furu
+  def roll
     deme1 = Dice.roll
     deme2 = Dice.roll
     deme3 = Dice.roll
   end
 
+  def assert(demes)
+    case demes.sort
+    end
+  end
 end
 
 game = Game.new
