aa20b21744e985d1d95a2053167f5be0ec99e366
threetreeslight <ae06710@gmail.com>
2019-07-24 20:19:22 +0900

    tada feat. yubachiri

--- game.rb
+++ game.rb
@@ -1,8 +1,17 @@
 require_relative "dice.rb"
 
 class Game
+  def main
+    puts furu
+  end
+
   def furu
+    deme1 = Dice.roll
+    deme2 = Dice.roll
+    deme3 = Dice.roll
   end
 
 end
 
+game = Game.new
+game.main
